75c5b33b1634fd98633e2b95a073a99263b3cbfd
chp320 <chp320@naver.com>
2023-12-06 21:48:31 +0900

    잘못된 다운캐스팅 오류 케이스

---
+++ app/src/main/java/doitjava/chap16/Solution1_UsingObject.java
@@ -0,0 +1,39 @@
+package doitjava.chap16;
+
+/**
+ * 최상위 클래스인 Object 클래스를 사용해서 다양한 객체를 저장함.
+ * 단점) 객체를 get 할 때는 Object 타입으로 가져올 것이기 때문에 반드시 "다운 캐스팅"이 필요한데, 형변환을 잘못하는 경우 런타임 오류 발생할 것임! (오류 가능성 존재)
+ * -> 와!!! 컴파일 시 오류 뱉네!!
+ */
+
+class Apple {}
+class Pencil {}
+
+class Goods {
+  private Object object = new Object();
+  public Object getObject() {
+    return object;
+  }
+  public void setObject(Object object) {
+    this.object = object;
+  }
+}
+
+public class Solution1_UsingObject {
+
+  public static void main(String[] args) {
+    Goods goods1 = new Goods();
+    Goods goods2 = new Goods();
+
+    goods1.setObject(new Apple());
+    goods2.setObject(new Pencil());
+
+    Apple apple = (Apple) goods1.getObject();
+    Pencil pencil = (Pencil) goods2.getObject();
+
+    // // 잘못된 캐스팅
+    // Goods goods3 = new Goods();
+    // goods3.setObject(new Apple());
+    // Pencil pencil2 = (Pencil) goods3.getObject();
+  }
+}
