78f47b8066e060e96bd22817d898594dbae12d7f
chp320 <chp320@naver.com>
2023-12-06 23:17:20 +0900

    제너릭 메서드

--- app/src/main/java/doitjava/chap16/Solution2_Generic.java
+++ app/src/main/java/doitjava/chap16/Solution2_Generic.java
@@ -3,7 +3,7 @@ package doitjava.chap16;
 class Apple3{}
 class Pencil3{}
 // 제너릭 클래스 정의
-class Goods<T> {
+class Goods2<T> {
   private T t;
   public T get() {
     return t;
@@ -16,13 +16,13 @@ class Goods<T> {
 public class Solution2_Generic {
   public static void main(String[] args) {
     // Goods 이용해서 Apple 객체 추가/가져오기
-    Goods<Apple3> goods1 = new Goods<>();
+    Goods2<Apple3> goods1 = new Goods2<>();
     goods1.set(new Apple3());
     Apple3 apple3 = goods1.get();
     System.out.println(apple3);
 
     // Goods 이용해서 Pencil 객체 추가/가져오기
-    Goods<Pencil3> goods2 = new Goods<>();
+    Goods2<Pencil3> goods2 = new Goods2<>();
     goods2.set(new Pencil3());
     Pencil3 pencil3 = goods2.get();
     System.out.println(pencil3);
