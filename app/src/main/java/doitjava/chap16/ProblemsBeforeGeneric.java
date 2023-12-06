95074a975965155a7b57d8c89c61e611c6cfaee6
chp320 <chp320@naver.com>
2023-12-06 22:14:40 +0900

    미사용 local 변수 제거

--- app/src/main/java/doitjava/chap16/ProblemsBeforeGeneric.java
+++ app/src/main/java/doitjava/chap16/ProblemsBeforeGeneric.java
@@ -31,12 +31,12 @@ public class ProblemsBeforeGeneric {
     // Goods1을 통해서만 Apple 객체 추가/가져오기
     Goods1 goods1 = new Goods1();
     goods1.setApple(new Apple());
-    Apple apple = goods1.getApple();
+    goods1.getApple();
 
     // Goods2를 통해서만 Pencil 객체 추가/가져오기
     Goods2 goods2 = new Goods2();
     goods2.setPencil(new Pencil());
-    Pencil pencil = goods2.getPencil();
+    goods2.getPencil();
 
   }
 }
